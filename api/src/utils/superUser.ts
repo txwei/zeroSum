5947a7ae160cd396e8ee37fb49dc8638468d0fb9
Tianxiao Wei <tianxiao.wei@mongodb.com>
2026-01-03 03:39:45 -0800

    Fix super user check: use username 'twei' instead of 'wtx'
    Username is 'twei', display name is 'wtx'

--- api/src/utils/superUser.ts
+++ api/src/utils/superUser.ts
@@ -2,12 +2,12 @@ import { AuthRequest } from '../middleware/auth';
 
 /**
  * Check if the current user is a super user (admin of all groups)
- * Super user username: "wtx"
+ * Super user username: "twei" (display name: "wtx")
  */
 export const isSuperUser = (req: AuthRequest): boolean => {
   if (!req.user) {
     return false;
   }
-  return req.user.username === 'wtx';
+  return req.user.username === 'twei';
 };
 
